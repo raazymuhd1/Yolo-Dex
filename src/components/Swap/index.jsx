dac5b487fe7a584b02f59c9389c74b368d33f8f3
muhammadraazy <muhammadrazi51@gmail.com>
2025-01-21 08:53:22 -0800

    added an approve & swap buttons

--- src/components/Swap/index.jsx
+++ src/components/Swap/index.jsx
@@ -7,7 +7,7 @@ import { VscSettings } from "react-icons/vsc";
 const Swap = () => {
   return (
     <div className='w-full h-full flex justify-center items-center'>
-       <div className="flex w-[40%] mt-[80px] min-h-[400px] flex-col rounded-[15px] p-[20px] bg-[#cd85af]"> 
+       <div className="flex w-[40%] mt-[80px] min-h-[500px] flex-col rounded-[15px] p-[20px] bg-[#cd85af]"> 
           <div className="flex items-center justify-between w-full"> 
               <h2 className='text-[1.5rem] text-black text-[#fff]'> Swap </h2>
               <VscSettings className="text-[30px] cursor-pointer" />
@@ -20,7 +20,10 @@ const Swap = () => {
                   <h4 className="font-semibold"> 1 WETH = 0.000 USDT ($-) </h4>
                   <h4  className="font-semibold"> Fee: -- </h4>
                </div>
-               <CustomButton title={"Swap"} styles="bg-[#cd85af] text-secondary btn_trans"  />
+               <div className="w-[80%] mx-auto flex items-center gap-[20px]"> 
+                  <CustomButton title={"Approve USDT"} styles="w-[50%] bg-secondary text-main btn_trans"  />
+                  <CustomButton title={"Swap"} styles="w-[50%] bg-secondary text-main btn_trans"  />
+               </div>
            </div>
        </div>
     </div>
