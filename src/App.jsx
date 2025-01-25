1ddd0aa651f77465be2dd2d76a5f0ec3c20f7fb1
muhammadraazy <muhammadrazi51@gmail.com>
2025-01-25 07:44:31 -0800

    setup a route for bridge, stake, and pool pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { Header, Swap, TestSwap, Footer } from './components'
+import { Bridge, Pool, Stake } from "./pages"
 import SwapContextProvider from "./components/ContextApi"
 
 const App = () => {
@@ -9,6 +10,9 @@ const App = () => {
        <Routes>
           <Route path='/' element={<Swap />} />
           <Route path='/test-swap' element={<TestSwap />} />
+          <Route path='/bridge' element={<Bridge />} />
+          <Route path='/stake' element={<Stake />} />
+          <Route path='/pool' element={<Pool />} />
        </Routes>
        <Footer />
     </SwapContextProvider>
