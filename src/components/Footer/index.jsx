3dc5671705e1e9240838e3eb36c7b619e39f9b8a
raazymuhd1 <muhammadrazi51@gmail.com>
2025-02-15 22:38:28 +0700

    changed the footer bg-color

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full mt-[170px] h-[60px] main-theme flex justify-between p-[15px]">
-         <h2 className="font-bold text-[16px] md:text-[20px] lg:text-[24px]"> PinkySwap </h2>
-         <h4> @copyrights <strong> pinkyswap </strong> | 2025 </h4>
+    <footer className="w-full mt-[170px] h-[60px] bg-mainAlt flex justify-between p-[15px]">
+         <h2 className="font-bold text-textWhite text-[16px] md:text-[20px] lg:text-[24px]"> PinkySwap </h2>
+         <h4 className="font-medium text-secondaryAlt"> @copyrights <strong> pinkyswap </strong> | 2025 </h4>
     </footer>
   )
 }
