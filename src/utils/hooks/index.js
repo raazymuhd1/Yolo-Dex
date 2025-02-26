8e00038e7f5b5756675ac879ecd93d98858a3db7
raazymuhd1 <muhammadrazi51@gmail.com>
2025-02-26 17:03:15 +0700

    handling trade quoting for exactInput

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,9 +1,60 @@
-const handlingTradeQuoting = (tokenIn, tokenOut, poolFactoryAddr, poolFee) => {
+import { computePoolAddress } from '@uniswap/v3-sdk' 
+import { ethers } from 'ethers'
+import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+import Quoter from '@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json'
+
+// #TODO: adding more details about the tokens, like, name, address, decimals, etc
+
+const handlingTradeQuoting = async(tokenIn, tokenOut, amount, poolFactoryAddr, poolFee, rpcUrl) => {
+
+    const CurrentConfig = {
+        rpc: {
+            local: 'http://localhost:8545',
+            mainnet: rpcUrl,
+        },
+        tokens: {
+            in: tokenIn,
+            amountIn: amount,
+            out: tokenOut,
+            poolFee: poolFee,
+        },
+    }
 
     const currentPoolAddress = computePoolAddress({
-    factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-    tokenA: CurrentConfig.tokens.in,
-    tokenB: CurrentConfig.tokens.out,
-    fee: CurrentConfig.tokens.poolFee,
-})
+        factoryAddress: poolFactoryAddr,
+        tokenA: CurrentConfig.tokens.in,
+        tokenB: CurrentConfig.tokens.out,
+        fee: CurrentConfig.tokens.poolFee,
+    })
+
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+    const currentPoolContract = new ethers.Contract(
+        currentPoolAddress,
+        IUniswapV3PoolABI.abi,
+        provider
+    )
+
+    const [token0, token1, fee, liquidity, slot0] = await Promise.all([
+        poolContract.token0(),
+        poolContract.token1(),
+        poolContract.fee(),
+        poolContract.liquidity(),
+        poolContract.slot0(),
+    ])
+    const quoterContract = new ethers.Contract(
+        QUOTER_CONTRACT_ADDRESS,
+        Quoter.abi,
+        provider
+    )
+
+    const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+        token0,
+        token1,
+        fee,
+        fromReadableAmount(
+            CurrentConfig.tokens.amountIn,
+            CurrentConfig.tokens.in.decimals
+        ).toString(),
+        0
+    )
 }
