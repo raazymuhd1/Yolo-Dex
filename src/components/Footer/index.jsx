613f8be4282894566fd6d3de880f04cde5129942
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-14 14:08:29 +0700

    change the project's title on the header & footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="w-[90%] mt-[130px] h-[60px] bg-[var(--light-dark)] flex justify-between p-[15px] rounded-[10px] mx-auto relative bottom-0 border-t-[1px] border-t-secondary">
-         <h2 className="font-bold text-[var(--orange-bg)] text-[16px] md:text-[20px] lg:text-[24px]"> PinkySwap </h2>
-         <h4 className="font-medium text-[var(--grey)]"> @copyrights <strong> pinkyswap </strong> | 2025 </h4>
+         <h2 className="font-bold text-[var(--orange-bg)] text-[16px] md:text-[20px] lg:text-[24px]"> YoloSwap </h2>
+         <h4 className="font-medium text-[var(--grey)]"> @copyrights <strong> YoloSwap </strong> | 2025 </h4>
     </footer>
   )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="min-h-[100px] w-full sticky top-0 z-[9] bg-[var(--light-dark)] card_shadows border-b-[1px] border-b-secondary md:py-0 py-[5px]">
         <nav className="md:w-[90%] w-full h-full mx-auto flex lg:flex-row flex-col gap-[.7rem] items-center justify-between ">
             {/* <div className="flex md:flex-row flex-col w-[60%] md:justify-between items-center md:gap-[60px]"> */}
-              <h2 className=" font-extrabold text-[var(--orange-bg)] text-[clamp(2rem,2vmax,2.4rem)]"> PinkySwap </h2>
+              <h2 className=" font-extrabold text-[var(--orange-bg)] text-[clamp(2rem,2vmax,2.4rem)]"> YoloSwap </h2>
               <Links />
             {/* </div>  */}
             {/* <Networks /> */}
