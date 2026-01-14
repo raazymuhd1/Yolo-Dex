001e6cc007adf53e4f5a77d6248f02e44c951c94
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-14 11:03:58 +0700

    handle network selection on bridge page

--- src/components/Bridge/BridgeChains.jsx
+++ src/components/Bridge/BridgeChains.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { networkLists } from "../../constants"
 import { MdArrowBack  } from "react-icons/md";
 
-const BridgeChains = ({showChains, setShowChains}) => {
+const BridgeChains = ({showChains, setShowChains, setSelectedNetwork}) => {
   return (
     <section 
       className={`w-full h-full absolute inset-0 flex flex-col gap-[20px] ${showChains ? "translate-x-[0px] opacity-1" : "translate-x-[760px] opacity-0 "} transition-all duration-[1s] bg-mainAlt p-[15px] rounded-[15px]`}>
@@ -11,12 +11,18 @@ const BridgeChains = ({showChains, setShowChains}) => {
          <h4 className="text-[14px] md:text-[16px] lg:text-[18px] font-semibold"> Select Chain </h4>
        </aside>
 
-       {/* token lists */}
+       {/* network lists */}
        <div className="w-full flex flex-col gap-[15px] overflow-y-auto"> 
            { networkLists.map(network => (
-              <div key={network.id} 
-                 className="flex-row-center w-full justify-between bg-secondary cursor-pointer text-[#fff] p-[10px] rounded-[30px] overflow-hidden transition-[background] duration-500 hover:bg-secondaryAlt hover:text-textOrange"> 
-                 <aside className="flex-row-center gap-[10px]"> 
+              <div 
+                  onClick={() => {
+                     setShowChains(false)
+                     setSelectedNetwork(network)
+                  }}
+                  key={network.id} 
+                 className="flex-row-center w-full justify-between bg-secondary cursor-pointer text-[#fff] p-[10px] rounded-[30px] overflow-hidden transition-[background] duration-500 hover:bg-[var(--orange-bg)]"> 
+                 <aside 
+                  className="flex-row-center gap-[10px]"> 
                      <img src={network.logo} alt="token-logo" className="w-[35px] h-[35px] rounded-[50%]" />
                     <p className="uppercase text-[.8vmax] font-bold"> {network.name} </p>
                  </aside>
--- src/components/Bridge/BridgeNetworks.jsx
+++ src/components/Bridge/BridgeNetworks.jsx
@@ -6,18 +6,19 @@ import BridgeChains from "./BridgeChains"
 
 const BridgeNetworks = () => {
     const [showNetworks, updateShowNetworks] = useState(false)
+    const [selectedNetwork, setSelectedNetwork] = useState(networkLists[0])
 
   return (
     <div className="w-[25%]">
         <aside 
             onClick={() => updateShowNetworks(!showNetworks)}
-            className="flex-row-center w-[fit-content] border-[1px] border-secondaryAlt cursor-pointer py-[4px] bg-mainAlt rounded-[10px]">
-            <img src={eth} alt="network" className="w-[25px] h-[25px] rounded-[50%] object-cover" />
-            <h3 className="font-bold text-[.7vmax] text-[#fff] uppercase"> Ethereum </h3>
+            className="flex-row-center min-w-[150px] gap-[5px] border-[1px] border-[var(--orange-bg)] cursor-pointer py-[4px] bg-mainAlt rounded-[10px]">
+            <img src={selectedNetwork?.logo} alt="network" className="w-[25px] h-[25px] rounded-[50%] object-cover" />
+            <h3 className="font-bold text-[clamp(.7rem,1vmax,.9rem)] text-[#fff] uppercase"> {selectedNetwork?.name} </h3>
             <MdKeyboardArrowDown size={20} className="text-[#fff]" />
         </aside>
         
-        <BridgeChains showChains={showNetworks} setShowChains={updateShowNetworks} />
+        <BridgeChains setSelectedNetwork={setSelectedNetwork} showChains={showNetworks} setShowChains={updateShowNetworks} />
     </div>
   )
 }
