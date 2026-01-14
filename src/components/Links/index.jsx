d647c774bdf2e5e652154ad34d9ce90702a63396
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-14 14:19:40 +0700

    fixing the navbar links font-size

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -3,11 +3,11 @@ import { navlinks } from "../../constants"
 
 const Links = () => {
   return (
-    <ul className="flex items-center md:gap-[30px] bg-secondary rounded-[15px] md:p-[10px] p-[4px] overflow-hidden">
+    <ul className="w-max flex items-center md:gap-[30px] bg-secondary rounded-[15px] md:p-[10px] p-[4px] overflow-hidden">
         { navlinks.map(link => (
             <NavLink 
               key={link.id} to={link.url} className={({isActive}) =>
-                  isActive ? "active_link bg-[var(--orange-bg)] text-[var(--white)]" : "active_link text-textWhite text-[clamp(1rem,1vmax,1.1rem)]"}> {link.title}
+                  isActive ? "active_link bg-[var(--orange-bg)] text-[var(--white)]" : "active_link text-textWhite px-[10px] py-[2px]"}> {link.title}
             </NavLink>
         )) }
     </ul>
